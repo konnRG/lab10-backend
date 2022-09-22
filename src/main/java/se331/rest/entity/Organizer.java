f3b3f15be6b385c3f8fe70ae1c65cf2425b6047d
konnRG <burussakorn_ruangon@elearning.cmu.ac.th>
2022-09-22 17:22:45 +0700

    2.2 Setup relationships in entities.

--- src/main/java/se331/rest/entity/Organizer.java
+++ src/main/java/se331/rest/entity/Organizer.java
@@ -1,9 +1,20 @@
 package se331.rest.entity;
 
-import java.util.List;
+import lombok.*;
 
+import javax.persistence.*;
+import java.util.List;
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Organizer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Exclude
     Long id;
     String name;
+    @OneToMany(mappedBy = "organizer")
     List<Event> ownEvents;
 }
